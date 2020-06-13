9289bd3310df87c6fc56e21acf4c62cd948926bb
Lucas Vallejos <lvallejos@baufest.com>
2020-06-13 19:09:20 -0300

    Control de Excepciones

---
+++ 01-PrincipiosYFundamentosDePython/13-Excepciones.py
@@ -0,0 +1,41 @@
+﻿#Podemos atrapar los errores que pueden ir surgiendo a lo largo de la ejecucion de nuestro programa
+cantidadDeIntentos = 0
+while(True):
+    try:
+        variable = float(input("Ingrese un valor numerico: "))       
+    except:
+        cantidadDeIntentos +=1
+        print("\n\tERROR: Se solicitó ingresar un numero\n")
+    else:
+        doble = variable * 2
+        print("El doble de su variable ", variable, " es -> ", doble)  
+        break
+    #Esto ultimo se ejecuta sin importar si surgio error o no
+    finally:
+        print(cantidadDeIntentos)
+
+#No solo podemos agarrar uno solo, sino muchos
+
+while(True):
+    try:
+        variable = float(input("Ingrese un valor numerico por el cual desea dividir el numero 43: "))
+        division = 43//variable
+    except TypeError:
+        cantidadDeIntentos +=1
+        print("\n\tERROR: Se solicitó ingresar un numero\n")
+    except ValueError:
+        cantidadDeIntentos +=1
+        print("\n\tERROR: Se solicitó ingresar un numero\n")
+    except ZeroDivisionError:
+        cantidadDeIntentos +=1
+        print("\n\tERROR: No se puede dividir por 0\n")
+    except Exception as e:
+        cantidadDeIntentos +=1
+        print("\n\tERROR: ",type(x).__name__ ,"\n")
+    else:
+        print("Dividir 43 por ", variable, " es -> ", division)  
+        break
+    finally:
+        print(cantidadDeIntentos)
+
+##Averiguar sobre raise()
