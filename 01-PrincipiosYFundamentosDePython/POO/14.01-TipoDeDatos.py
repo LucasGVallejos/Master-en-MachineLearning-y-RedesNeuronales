4ada39c4ea18fe56503ea8f764b599c58a948482
Lucas Vallejos <lvallejos@baufest.com>
2020-06-13 19:50:25 -0300

    Atributos y Clases en Pythoon

---
+++ 01-PrincipiosYFundamentosDePython/POO/14.01-TipoDeDatos.py
@@ -0,0 +1,11 @@
+﻿#Creamos nuestra Clase 'Auto' vacia
+class Auto:
+    pass
+    # a class with no methods (yet)
+    #pass is a null operation -- when it is executed, nothing happens.
+auto = Auto()
+
+def Auto():
+    pass
+
+print("A diferencia del tipo de las funciones --> ", type(Auto), "<-- \nLas clases presentan el siguiente tipo -->", type(auto), "<--")
---
+++ 01-PrincipiosYFundamentosDePython/POO/14.02-AtributosYClases.py
@@ -0,0 +1,24 @@
+﻿#Creamos nuestra Clase 'Auto' vacia
+class Auto:
+    pass
+    # a class with no methods (yet)
+    #pass is a null operation -- when it is executed, nothing happens.
+
+#Podemos ir agregandole atributos de la siguiente manera
+auto.color = 'Rojo'
+auto.puertas = 2
+auto.dueño = input("\nIngrese Dueño del auto: ")
+
+print("\nEl auto de ", auto.dueño, " posee ",auto.puertas, "puertas y es de color ",auto.color)
+
+#O podemos agregarle atributos cuando se define la propia Clase
+class Consecionaria:    
+    #La funcion __init__ refleja un constructor
+    def __init__(self, cantidadDeAutos):
+        self.cantidadDeAutos = cantidadDeAutos
+        print("Se creo una Consecionaria con una cantidad de ",self.cantidadDeAutos," autos!\n")
+    def AgregarNuevoAuto(self):
+        self.cantidadDeAutos +=1
+        print("La nueva cantidad de autos es -> ", self.cantidadDeAutos)
+        
+consecionaria = Consecionaria(10)  
